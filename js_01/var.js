6b51ab07a4aecfe8c67465478dc17fee7e29a2e0
Monu Kumar <117930255+2002MONU@users.noreply.github.com>
2024-09-05 16:16:20 +0000

    add data varibles

---
+++ js_01/var.js
@@ -0,0 +1,18 @@
+const accountId = 12211;
+let accountEmail = "monu@gmal.com";
+var accountPassword = "123456";
+
+accountCity = "Jaipur";
+accountPassword= "12345";
+accountEmail= "sonu@gmail.com";
+let accountState; //undefined varibles
+ //accountId = 2;  not allowed because const already defir 
+/*
+prefer not to use var 
+because of use issue in block scope and functional scope 
+
+*/
+console.log(accountId);
+console.log([accountPassword,accountEmail,accountCity]);
+
+ 
