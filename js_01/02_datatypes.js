d8f6f7fb9e75a4fe5e387edf6e098c50a6ef8339
Monu Kumar <117930255+2002MONU@users.noreply.github.com>
2024-09-05 16:16:46 +0000

    add varibles

---
+++ js_01/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) // we are using node.js , not brower
+
+// number => 2 to power  53
+// bigint 
+// string => '' , " "
+// bloolean => true , false 
+// null = => standalone value
+// undefined => 
+    // symbol => 
+
+        // object 
+        // 
+
+        console.log(typeof"histesh");
+
+        console.log(typeof null); // object ha 
+        console.log( typeof undefined); //  type ha 
+        
+        
