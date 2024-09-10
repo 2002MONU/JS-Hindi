a142054a8afaded34ac45a88f2c1fe04b2874c76
Monu Kumar <117930255+2002MONU@users.noreply.github.com>
2024-09-10 16:00:30 +0000

    conversationn or data types

---
+++ js_01/02_conver.js
@@ -0,0 +1,28 @@
+// let score = "33";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+//  let valueInNumber = Number(score);
+//  console.log(typeof valueInNumber);
+
+ // "33 " => 33 
+ // "22afn"= > NaN
+ /// true =>1 , false = 0;
+
+ let isLoggedIn = 1
+
+ let booleanIsLoogedIn = Boolean(isLoggedIn)
+ console.log(booleanIsLoogedIn);
+ 
+
+ // 1 => true ; 0 => false ;
+ // " "=> false;
+ // "Monu" =>false;
+
+ let someNumber = 33
+
+ let stringNumber = String(someNumber)
+ console.log(stringNumber);
+ 
+
+ 
