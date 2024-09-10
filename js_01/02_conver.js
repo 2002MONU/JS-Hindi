2b58f9ad714438c3d7b9ee28cb589fde06da9c9f
Monu Kumar <117930255+2002MONU@users.noreply.github.com>
2024-09-10 16:22:45 +0000

    operatores

--- js_01/02_conver.js
+++ js_01/02_conver.js
@@ -1,28 +1,57 @@
-// let score = "33";
+// // let score = "33";
 
-// console.log(typeof score);
-// console.log(typeof(score));
-//  let valueInNumber = Number(score);
-//  console.log(typeof valueInNumber);
+// // console.log(typeof score);
+// // console.log(typeof(score));
+// //  let valueInNumber = Number(score);
+// //  console.log(typeof valueInNumber);
 
- // "33 " => 33 
- // "22afn"= > NaN
- /// true =>1 , false = 0;
+//  // "33 " => 33 
+//  // "22afn"= > NaN
+//  /// true =>1 , false = 0;
 
- let isLoggedIn = 1
+//  let isLoggedIn = 1
 
- let booleanIsLoogedIn = Boolean(isLoggedIn)
- console.log(booleanIsLoogedIn);
+//  let booleanIsLoogedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoogedIn);
  
 
- // 1 => true ; 0 => false ;
- // " "=> false;
- // "Monu" =>false;
+//  // 1 => true ; 0 => false ;
+//  // " "=> false;
+//  // "Monu" =>false;
 
- let someNumber = 33
+//  let someNumber = 33
 
- let stringNumber = String(someNumber)
- console.log(stringNumber);
+//  let stringNumber = String(someNumber)
+//  console.log(stringNumber);
  
 
- 
+ //  ===============operations ====================
+
+//  let value = 3 
+//  let negValue = -value
+//   console.log(negValue)
+
+// let str1 = "Hello"
+// let str2 = " Monu"
+
+// let str3 = str1+str2
+
+// console.log(str3);
+
+// console.log("1" + 2)
+// console.log(1 + "2");
+// console.log("1" +2 +2);
+// console.log(1 + 2 + "2");
+
+// console.log( true);
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
+
+
+
