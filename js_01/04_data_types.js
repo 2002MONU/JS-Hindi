82b753c467e858491a6530c1d55d17db24251187
Monu Kumar <117930255+2002MONU@users.noreply.github.com>
2024-09-11 16:58:49 +0000

    data type

---
+++ js_01/04_data_types.js
@@ -0,0 +1,25 @@
+ 
+ 
+ // Primitive data types 
+
+ //  7 types
+ // String , Number , Boolean , Null , undefined , Symbol , BigInt
+ 
+
+
+ 
+ // Reference (NOn primitve )
+ // arrays , Objects , functions 
+ const heros = ["shaktiman", "Nagraj" ,"Doga"];
+
+ let myObj = {
+    name: "Monu",
+    age: 22,
+ }
+
+  const myFunction = function(){
+      console.log('Hello Monu');
+      
+ }
+
+ console.log(typeof of (heros));
