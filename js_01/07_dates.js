7c986308392b9d7fbbee4396b295677c616418e2
Monu Kumar <117930255+2002MONU@users.noreply.github.com>
2024-09-17 16:47:46 +0000

    dates in js

---
+++ js_01/07_dates.js
@@ -0,0 +1,36 @@
+ let myDate = new Date();
+
+//  console.log(myDate.toString());
+//  console.log(myDate.toDateString());
+//  console.log(myDate.toLocaleDateString());
+
+//  console.log(typeof myDate);
+
+//  let myCreateDate = new Date(2023 , 0 , 23);
+ let myCreateDate = new Date("2024-01-12")
+//  console.log(myCreateDate.toDateString());
+//  console.log(myCreateDate.toLocaleDateString());
+//  console.log(myCreateDate.toLocaleDateString());
+
+// let myTimeStamp = Date.now();
+
+// console.log(myTimeStamp);
+// console.log(myTimeStamp);
+
+let newDate = new Date();
+
+// console.log(newDate);
+// console.log(newDate.getMonth());
+// console.log(newDate.getDay());
+
+newDate.toLocaleString('default',{
+    weekday:"long",
+    timeZone:"timeZone"
+});
+
+
+
+
+ 
+ 
+ 
