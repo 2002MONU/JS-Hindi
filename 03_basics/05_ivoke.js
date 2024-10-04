d977a30e1c8f4efed76ea2b29ae344af1c7260cb
Monu Kumar <117930255+2002MONU@users.noreply.github.com>
2024-10-04 16:30:30 +0000

    IVFE

---
+++ 03_basics/05_ivoke.js
@@ -0,0 +1,13 @@
+
+
+ ( function chai(){  // name IVFE
+    console.log('DB Connection');
+    
+  })();
+
+  (  (name) => {  /// unname IVFE
+    console.log(`Monu  ${name}`);
+    
+  })('saini')
+
+  // IVFE = immdediate run  and golbal scope ka variable ka polution ko hatane ka liya 
