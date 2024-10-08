619e450716e7e65d56300d55234e0b708ab8b311
Monu Kumar <117930255+2002MONU@users.noreply.github.com>
2024-10-08 17:13:18 +0000

    itteration

---
+++ iteration/03.js
@@ -0,0 +1,13 @@
+
+const arr = [1,0, 2 ,4 ,5 ,6 , 7 , 8];
+
+for (const num of arr) {
+   // console.log(num);
+    
+}
+
+const greeting = "Hello Monu";
+for (const greet of greeting) {
+     console.log(greet);
+     
+}
